bd71c3eb9246bbec25106537f5b3ca6c4520d878
Rastko <rastko.vukasinovic@gmail.com>
2022-01-13 16:54:52 +0100

    solve the maze

--- maze.py
+++ maze.py
@@ -115,3 +115,57 @@ class Maze():
             except IndexError:
                 continue
         return result
+
+
+    def solve(self):
+        """Find a solution to maze, if it exists"""
+
+        # keep the track of number of states explored
+        self.num_explored = 0
+
+        # Initialize frontier ot the starting position
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        # initialize explored set
+        self.explored = set()
+
+        # loop
+        while True:
+
+            # if nothing is left in the frontier, then no path
+            if frontier.empty():
+                raise Exception("no solution")
+
+            # Chose a node from the frontier
+            node = frontier.remove()
+            self.num_explored +=1
+
+            # do we have a solution
+            if node.state == self.goal:
+                actions = []
+                cells = []
+
+                # Follow parent nodes to find solution
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+        
+            # if it is not solution, just mark it as explored
+            self.explored.add(node.state)
+
+            # Add neighbors to frontier
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+    
+
+             
